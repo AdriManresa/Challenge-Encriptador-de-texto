796b387acad80d1d84b48f9b8af3e3a05f1ac315
AdriManresa <adrimanresa62@gmail.com>
2024-08-12 20:36:41 -0300

    Mejorando el codigo

--- js/encriptador.js
+++ js/encriptador.js
@@ -1,95 +1,86 @@
-function encriptar(traduccion){
-    document.querySelector("#warning").removeAttribute("style");
-    var textarea = document.querySelector("#texto");
+function encriptar(traduccion) {
+    const warning = document.querySelector("#warning");
+    const textarea = document.querySelector("#texto");
     const texto = textarea.value;
-    var area_default = document.querySelector("#default");
-    var area_result = document.querySelector("#result");
-    var texto_out = document.querySelector("#texto_out");
-    if (texto != ""){
-        var out = ""
-        for(var i=0; i < texto.length; i++){
-            if(((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')){
-                document.querySelector("#warning").style.color = "red";
-                document.querySelector("#warning").style.fontSize = "16px";
-                return;
-            }
-            else if((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == ""){
-                area_default.classList.remove("invisible");
-                area_result.classList.add("invisible");
-                return;
-            }
-            if(texto[i] == 'a'){
-                out += traduccion["a"] ;
-            }
-            else if(texto[i] == 'e'){
-                out += traduccion["e"];
-            }
-            else if(texto[i] == 'i'){
-                out += traduccion["i"]; 
-            }
-            else if(texto[i] == 'o'){
-                out += traduccion["o"]; 
-            }
-            else if(texto[i] == 'u'){
-                out += traduccion["u"]; 
-            }
-            else{
-                out += texto[i];
-            }
-        }
-
-        area_default.classList.add("invisible");
-        area_result.classList.remove("invisible");
-        texto_out.innerHTML = out;
-    }
-
-    return;
-
-}
-
-function desencriptar(traduccion){
-    document.querySelector("#warning").removeAttribute("style");
-    var textarea = document.querySelector("#texto");
-    var texto = textarea.value;
-    var area_default = document.querySelector("#default");
-    var area_result = document.querySelector("#result");
-    var texto_out = document.querySelector("#texto_out");
-    if (texto != ""){
-        for(var i=0; i < texto.length; i++){
-            if(((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')){
-                document.querySelector("#warning").style.color = "red";
-                document.querySelector("#warning").style.fontSize = "16px";
-                return;
-            }
-            else if((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == ""){
-                area_default.classList.remove("invisible");
-                area_result.classList.add("invisible");
-                return;
-            }
-        }
-        area_default.classList.add("invisible");
-        area_result.classList.remove("invisible");
-        texto = texto.replace(new RegExp(traduccion["a"], "g"), "a");
-        texto = texto.replace(new RegExp(traduccion["e"], "g"), "e");
-        texto = texto.replace(new RegExp(traduccion["i"], "g"), "i");
-        texto = texto.replace(new RegExp(traduccion["o"], "g"), "o");
-        texto = texto.replace(new RegExp(traduccion["u"], "g"), "u");
-        texto_out.innerHTML = texto;
-    }
-    return;
-}
-
-function clipboard(){
+    const area_default = document.querySelector("#default");
+    const area_result = document.querySelector("#result");
     const texto_out = document.querySelector("#texto_out");
-    navigator.clipboard.writeText(texto_out.value);
+
+    warning.removeAttribute("style");
+    if (texto.trim() === "") {
+        area_default.classList.remove("invisible");
+        area_result.classList.add("invisible");
+        return;
+    }
+
+    if (/[^a-z\s]/.test(texto)) {
+        warning.style.color = "red";
+        warning.style.fontSize = "16px";
+        return;
+    }
+
+    let out = "";
+    for (let i = 0; i < texto.length; i++) {
+        if (traduccion[texto[i]]) {
+            out += traduccion[texto[i]];
+        } else {
+            out += texto[i];
+        }
+    }
+
+    area_default.classList.add("invisible");
+    area_result.classList.remove("invisible");
+    texto_out.innerHTML = out;
+}
+
+function desencriptar(traduccion) {
+    const warning = document.querySelector("#warning");
+    const textarea = document.querySelector("#texto");
+    let texto = textarea.value;
+    const area_default = document.querySelector("#default");
+    const area_result = document.querySelector("#result");
+    const texto_out = document.querySelector("#texto_out");
+
+    warning.removeAttribute("style");
+    if (texto.trim() === "") {
+        area_default.classList.remove("invisible");
+        area_result.classList.add("invisible");
+        return;
+    }
+
+    if (/[^a-z\s]/.test(texto)) {
+        warning.style.color = "red";
+        warning.style.fontSize = "16px";
+        return;
+    }
+
+    area_default.classList.add("invisible");
+    area_result.classList.remove("invisible");
+
+    Object.keys(traduccion).forEach(key => {
+        texto = texto.replace(new RegExp(traduccion[key], "g"), key);
+    });
+
+    texto_out.innerHTML = texto;
+}
+
+function clipboard() {
+    const texto_out = document.querySelector("#texto_out");
+    navigator.clipboard.writeText(texto_out.innerHTML);
 }
 
 const enc = document.querySelector('#enc');
 const des = document.querySelector('#des');
 const copy = document.querySelector('#copiar');
 
-var traduccion = {"a": "ai", "e": "enter", "i": "imes", "o": "ober", "u": "ufat"};
+const traduccion = {
+    "a": "ai",
+    "e": "enter",
+    "i": "imes",
+    "o": "ober",
+    "u": "ufat"
+};
 
-enc.addEventListener( 'click', function() {encriptar(traduccion);} );
-des.addEventListener( 'click', function() {desencriptar(traduccion);} );
-copy.addEventListener( 'click', function() {clipboard();} );
+enc.addEventListener('click', () => encriptar(traduccion));
+des.addEventListener('click', () => desencriptar(traduccion));
+copy.addEventListener('click', clipboard);
